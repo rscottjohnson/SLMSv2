fae5a781925557be6896a3ce110e9e8562f31fba
rscottjohnson <sjohnson.wvu@gmail.com>
2020-10-24 17:24:10 -0400

    Added edit account form

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django import forms
+from .models import Profile
 
 class LoginForm(forms.Form):
   username = forms.CharField()
@@ -16,3 +17,13 @@ class UserRegistrationForm(forms.ModelForm):
     if cd['password'] !=cd['password2']:
       raise forms.ValidationError('Passwords don\'t match.')
     return cd['password2']
+
+class UserEditForm(forms.ModelForm):
+  class Meta:
+    model = User
+    fields = ('first_name', 'last_name', 'email')
+
+class ProfileEditForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ('guardian_name', 'guardian_email')
