2b432866a3ce3f8bd0fa97e9ea12a382fac33cbd
rscottjohnson <sjohnson.wvu@gmail.com>
2020-11-02 15:58:01 -0500

    Refactored list view to class-based view

--- snack/views.py
+++ snack/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Count
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -22,3 +23,10 @@ def count_list(request):
 def count_detail(request, year, month, day, count):
   count = get_object_or_404(Count, slug=count, status='published', publish__year=year, publish__month=month, publish__day=day)
   return render(request, 'snack/count/detail.html', {'count': count})
+
+# Refactor count_list view into the class-based view offered by Django
+class CountListView(ListView):
+  queryset = Count.published.all()
+  context_object_name = 'counts'
+  paginate_by = 8
+  template_name = 'snack/count/list.html'
