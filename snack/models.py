227ca88733892e56d8a0eb28eb3ef4ab46c6f758
rscottjohnson <sjohnson.wvu@gmail.com>
2020-11-02 12:50:46 -0500

    Added views

--- snack/models.py
+++ snack/models.py
@@ -6,6 +6,12 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
+# Custom manager
+class PublishedManager(models.Manager):
+  def get_queryset(self):
+    return super(PublishedManager, self).get_queryset().filter(status='published')
+
+# Model for Snack Counts
 class Count(models.Model):
   STATUS_CHOICES = (
     ('draft', 'Draft'),
@@ -32,6 +38,10 @@ class Count(models.Model):
   def __str__(self):
     return self.title
 
+  objects = models.Manager() # the default manager
+  published = PublishedManager() # custom manager
+
+# Model for Snack Foods
 class Food(models.Model):
   CATEGORY_CHOICES = (
     ('bars', 'Bars'),
@@ -52,3 +62,5 @@ class Food(models.Model):
   # default representation
   def __str__(self):
     return self.description
+
+  
--- snack/views.py
+++ snack/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Count
 
 # Create your views here.
+
+def count_list(request):
+  counts = Count.published.all()
+  return render(request, 'snack/count/list.html', {'counts': counts})
+
+def count_detail(request, year, month, day, count):
+  count = get_object_or_404(Count, slug=count, status='published', publish__year=year, publish__month=month, publish__day=day)
+  return render(request, 'snack/count/detail.html', {'count': count})
