f21e20feede8c7686252621259228aea477423d0
rscottjohnson <sjohnson.wvu@gmail.com>
2020-11-04 22:56:26 -0500

    Added starter tests

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,14 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from django.http.request import HttpRequest
+from django.urls import resolve
 from django.test import TestCase
+from account.views import dashboard
 
 # Create your tests here.
+
+class LoginPageTest(TestCase):
+
+  def test_root_url_resolves_to_dashboard_view(self):
+    found = resolve('/')
+    self.assertEqual(found.func, dashboard)
