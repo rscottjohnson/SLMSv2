f55e645bf6c038e67fc1555055cebd1a1e2f6617
rscottjohnson <sjohnson.wvu@gmail.com>
2020-11-09 12:12:10 -0500

    Added Test Scenario 1

--- account/tests.py
+++ account/tests.py
@@ -1,5 +1,7 @@
 from django.test import client
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from django.contrib.auth.models import User
 
@@ -7,6 +9,8 @@ import unittest
 
 # Organize tests into classes (which inherit from unittest.TestCase) here
 
+# OVERVIEW: AccountTest is an object for testing the account app of slms.
+# The AccountTest test test cases align with Test Scenario 1.
 class AccountTest(unittest.TestCase):
 
   def setUp(self):
@@ -14,13 +18,20 @@ class AccountTest(unittest.TestCase):
 
   def tearDown(self):
     self.browser.quit()
+    # Delete TC_1 user
+    tc1_user = User.objects.filter(email='chromeuser@example.com')
+    tc1_user.delete()
 
-  def test_register_new_user_log_in_log_out(self):
-    # Test user credentials
-    TEST_USERNAME = 'testuser2'
-    TEST_FIRST_NAME = 'Test'
-    TEST_EMAIL = 'testuser2@example.com'
-    TEST_PASSWORD = 'testuser2password'
+  # TC_1
+  def test_register_new_user_with_valid_inputs(self):
+    # MODIFIES: User model
+    # EFFECTS: Adds a new user to the User table when given valid inputs
+
+    # Test data
+    TEST_USERNAME = 'chromeuser'
+    TEST_FIRST_NAME = 'Chrome'
+    TEST_EMAIL = 'chromeuser@example.com'
+    TEST_PASSWORD = 'chromeuserpassword'
     # User visits SLMS register page
     self.browser.get('http://127.0.0.1:8000/account/register/')
     # The page title is 'Create account'
@@ -31,10 +42,47 @@ class AccountTest(unittest.TestCase):
     self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
     self.browser.find_element_by_name('password').send_keys(TEST_PASSWORD)
     self.browser.find_element_by_name('password2').send_keys(TEST_PASSWORD)
-    self.browser.find_element_by_name('create-account-button').send_keys(Keys.ENTER)  
-    # User visits SLMS homepage
-    self.browser.get('http://127.0.0.1:8000')
-    # The page title is now 'Log-in'
+    self.browser.find_element_by_name('create-account-button').send_keys(Keys.ENTER)
+
+    WebDriverWait(self.browser, 10).until(EC.url_matches('http://127.0.0.1:8000/account/register/'))
+    # Account now exists
+    self.assertTrue(User(username=TEST_USERNAME))
+
+  # TC_2
+  def test_register_new_user_with_invalid_inputs(self):
+    # EFFECTS: Messages to the user pointing out invalid inputs
+
+    # Test data
+    TEST_USERNAME = 'firefoxuser'
+    TEST_FIRST_NAME = ''
+    TEST_EMAIL = 'firefoxuser@example.com'
+    TEST_PASSWORD = ''
+    # User visits SLMS register page
+    self.browser.get('http://127.0.0.1:8000/account/register/')
+    # The page title is 'Create account'
+    self.assertIn('Create account', self.browser.title)
+    # Pass in the user credentials
+    self.browser.find_element_by_name('username').send_keys(TEST_USERNAME)
+    self.browser.find_element_by_name('first_name').send_keys(TEST_FIRST_NAME)
+    self.browser.find_element_by_name('email').send_keys(TEST_EMAIL)
+    self.browser.find_element_by_name('password').send_keys(TEST_PASSWORD)
+    self.browser.find_element_by_name('password2').send_keys(TEST_PASSWORD)
+    self.browser.find_element_by_name('create-account-button').send_keys(Keys.ENTER)
+    # Remain on 'Create account' page
+    self.assertIn('Create account', self.browser.title)
+    # Account not created
+    self.assertFalse(User.objects.filter(username=TEST_USERNAME))
+
+  # TC_3
+  def test_login_user_with_valid_inputs(self):
+    # EFFECTS: Redirects logged in user to the User Dashboard
+
+    # Test data
+    TEST_USERNAME = 'safariuser'
+    TEST_PASSWORD = 'safaripassword'
+    # User visits SLMS register page
+    self.browser.get('http://127.0.0.1:8000/')
+    # The page title is 'Log-in'
     self.assertIn('Log-in', self.browser.title)
     # Pass in the user credentials
     self.browser.find_element_by_name('username').send_keys(TEST_USERNAME)
@@ -46,11 +94,24 @@ class AccountTest(unittest.TestCase):
     self.browser.get('http://127.0.0.1:8000/account/logout/')
     # The page title is now 'Logged Out'
     self.assertIn('Logged Out', self.browser.title)
-    # Delete the test user that was created
-    test_user = User.objects.filter(username='testuser2')
-    test_user.delete()
-    # Test user should no longer be in the data set
-    self.assertFalse(User.objects.filter(username='testuser2').exists())
+
+  # TC_4
+  def test_login_user_with_invalid_inputs(self):
+    # EFFECTS: Redirects logged in user to the User Dashboard
+
+    # Test data
+    TEST_USERNAME = 'safariuser'
+    TEST_PASSWORD = ''
+    # User visits SLMS register page
+    self.browser.get('http://127.0.0.1:8000/')
+    # The page title is 'Log-in'
+    self.assertIn('Log-in', self.browser.title)
+    # Pass in the user credentials
+    self.browser.find_element_by_name('username').send_keys(TEST_USERNAME)
+    self.browser.find_element_by_name('password').send_keys(TEST_PASSWORD)
+    self.browser.find_element_by_name('log-in-button').send_keys(Keys.ENTER)
+    # The page title is still 'Log-in'
+    self.assertIn('Log-in', self.browser.title)
           
 if __name__ == '__main__':
   unittest.main(warnings='ignore')
