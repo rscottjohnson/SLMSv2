a4ed9843b8980c853930d64304504a1e72aeaefd
rscottjohnson <sjohnson.wvu@gmail.com>
2020-11-10 12:28:30 -0500

    Begin tests for Snack Food testing

--- snack/tests.py
+++ snack/tests.py
@@ -15,8 +15,69 @@ from django.utils import timezone
 
 # Create your tests here.
 
-# OVERVIEW: SnackTest is an object for testing the snack app of slms.
-# The SnackTest test cases align with Test Scenario 4.
+# OVERVIEW: SnackFoodTest is an object for testing the snack app of slms.
+# The SnackFoodTest test cases align with Test Scenario 3.
+class SnackFoodTest(unittest.TestCase):
+
+  def setUp(self):
+    self.browser = webdriver.Firefox()
+    # Create the test user
+    user = User.objects.create_superuser('fooduser', 'fooduser@example.com', 'fooduserpassword')
+
+  def tearDown(self):
+    self.browser.quit()
+    # Delete the test user
+    user = User.objects.filter(username='fooduser')
+    user.delete()
+
+  # TC_2
+  def test_create_food_as_logged_in_staff_user_with_valid_inputs(self):
+    # MODIFIES: Food model
+    # EFFECTS: Adds a new snack food to the Food table when given valid inputs
+
+    # Test user credentials
+    TEST_USERNAME = 'fooduser'
+    TEST_PASSWORD = 'fooduserpassword'
+    # Test lunch selection attributes
+    TEST_COUNT_TITLE = 'a'
+    letters = string.ascii_lowercase
+    TEST_COUNT_FOOD = Food.objects.get(pk=1).description
+    TEST_COUNT_QUANTITY = random.randint(1, 100)
+    TEST_COUNT_BODY = ''.join(random.choice(letters) for i in range(50))
+    TEST_COUNT_STATUS = 'Published'
+    TEST_COUNT_SLUG = str(slugify(TEST_COUNT_TITLE))
+    TEST_COUNT_YEAR = str(timezone.now().year)
+    TEST_COUNT_MONTH = str(timezone.now().month)
+    TEST_COUNT_DAY = str(timezone.now().day)
+    # Get the view
+    self.browser.get('http://127.0.0.1:8000/snack/create/')
+    # Pass in the credentials
+    self.browser.find_element_by_name('username').send_keys(TEST_USERNAME)
+    self.browser.find_element_by_name('password').send_keys(TEST_PASSWORD)
+    self.browser.find_element_by_name('password').send_keys(Keys.ENTER)
+    # Wait for the url to change
+    WebDriverWait(self.browser, 10).until(EC.url_matches('http://127.0.0.1:8000/snack/create/'))
+    # The page title has changed
+    self.assertIn('Create Snack Count', self.browser.title)
+    # Pass in the new count attributes
+    self.browser.find_element_by_name('title').send_keys(TEST_COUNT_TITLE)
+    self.browser.find_element_by_name('food').send_keys(TEST_COUNT_FOOD)
+    self.browser.find_element_by_name('quantity').send_keys(TEST_COUNT_QUANTITY)
+    self.browser.find_element_by_name('body').send_keys(TEST_COUNT_BODY)
+    self.browser.find_element_by_name('status').send_keys(TEST_COUNT_STATUS)
+    self.browser.find_element_by_name('create-count-button').send_keys(Keys.ENTER)
+    # Wait for the url to change
+    WebDriverWait(self.browser, 10).until(EC.url_matches('http://127.0.0.1:8000/snack/' + TEST_COUNT_YEAR + '/' + TEST_COUNT_MONTH + '/' + TEST_COUNT_DAY + '/' + TEST_COUNT_SLUG + '/'))
+    # The page title has changed
+    self.assertIn(TEST_COUNT_TITLE, self.browser.title)
+    # The new count is included
+    self.assertEqual(len(list(Count.objects.all().filter(title=TEST_COUNT_TITLE))), 1)
+
+  
+
+
+# OVERVIEW: SnackCountTest is an object for testing the snack app of slms.
+# The SnackCountTest test cases align with Test Scenario 4.
 class SnackCountTest(unittest.TestCase):
 
   def setUp(self):
@@ -192,7 +253,6 @@ class SnackCountTest(unittest.TestCase):
     # The page title has changed
     self.assertIn(TEST_COUNT_TITLE, self.browser.title)
     
-  
   # TC_6
   def test_edit_count_as_logged_in_staff_user_with_invalid_quantity(self):
     # EFFECTS:  Messages to the user pointing out invalid inputs
