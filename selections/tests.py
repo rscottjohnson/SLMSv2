fe573846c959baca47c6982286b1be3c6f528186
rscottjohnson <sjohnson.wvu@gmail.com>
2020-11-10 20:28:38 -0500

    Refactored TS2 tests

--- selections/tests.py
+++ selections/tests.py
@@ -18,20 +18,45 @@ class TestLunchSelection(TestCase):
   def tearDown(self):
     self.staff_user.delete()
 
+  # Selection list access
+
+  # TC_1
+  def test_anonymous_user_cannot_access_selection_list(self):
+    response = self.client.get(reverse("selections:list"))
+    self.assertRedirects(response, "/account/login/?next=/selections/")
+
+  # TC_2
+  def test_authenticated_non_staff_user_can_access_selection_list(self):
+    self.client.force_login(user=self.non_staff_user)
+    response = self.client.get(reverse("selections:list"))
+    self.assertEqual(response.status_code, 200)
+  
+  # TC_3
+  def test_authenticated_staff_user_can_access_selection_list(self):
+    self.client.force_login(user=self.staff_user)
+    response = self.client.get(reverse("selections:list"))
+    self.assertEqual(response.status_code, 200)
+
+  # Selection create access
+  
+  # TC_4
   def test_anonymous_user_cannot_access_create_selection(self):
     response = self.client.get(reverse("selections:create"))
     self.assertRedirects(response, "/account/login/?next=/selections/create/")
 
+  # TC_5
   def test_authenticated_non_staff_user_cannot_access_create_selection(self):
     self.client.force_login(user=self.non_staff_user)
     response = self.client.get(reverse("selections:create"))
     self.assertNotEqual(response.status_code, 200)
 
+  # TC_6
   def test_authenticated_staff_user_can_access_create_selection(self):
     self.client.force_login(user=self.staff_user)
     response = self.client.get(reverse("selections:create"))
     self.assertEqual(response.status_code, 200)
 
+  # TC_7
   def test_authenticated_staff_user_can_create_selection_with_valid_inputs(self):
     self.client.force_login(user=self.staff_user)
     selection_count = Selection.objects.all().count()
@@ -40,3 +65,10 @@ class TestLunchSelection(TestCase):
     letters = string.ascii_lowercase
     response = self.client.post('/selections/create/', {'lunch_type': random.choice(lt_choice_list), 'parent_attendance': random.choice(pa_choice_list), 'content': ''.join(random.choice(letters) for i in range(20))})
     self.assertEqual(Selection.objects.all().count(), selection_count + 1)
+
+  # TC_8
+  def test_authenticated_staff_user_can_create_selection_with_invalid_inputs(self):
+    self.client.force_login(user=self.staff_user)
+    selection_count = Selection.objects.all().count()
+    response = self.client.post('/selections/create/', {'lunch_type': '', 'parent_attendance': '', 'content': ''})
+    self.assertNotEqual(Selection.objects.all().count(), selection_count + 1)
